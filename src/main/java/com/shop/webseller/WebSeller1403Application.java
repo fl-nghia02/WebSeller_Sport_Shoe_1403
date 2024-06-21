f5e504d2915767e60697d83c1a405a34743cbe1c
fl-nghia02 <flower.hoangnghia02@gmail.com>
2024-06-21 22:22:00 +0700

    push origin project

---
+++ src/main/java/com/shop/webseller/WebSeller1403Application.java
@@ -0,0 +1,13 @@
+package com.shop.webseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSeller1403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSeller1403Application.class, args);
+	}
+
+}
